d25022baaa51eec0ef3350dd5fb3258c965223e6
pical444 <pcarrascaln@gmail.com>
2023-02-12 19:58:18 +0000

    Changes
    changes on background, text

--- sketch.js
+++ sketch.js
@@ -3,9 +3,52 @@ function setup() {
   }
   
   function draw() {
-    background(220);
+    background(mouseX, mouseY, 237);
     text("lala test", 20, 20)
-
+    text('I thought you told Marie to stop talking to you', 30, 30);
+    let r= random(10);
+    textSize(10+mouseX/width*30)
+    textSize()
+    text('l', 31,40, r);
+    text('o', 29,50, r);
+    text('v', 29,60, r);
+    text('e', 29,70, r);
+    text('y', 29,90, r);
+    text('o', 29,100, r);
+    text('u', 29,110, r);
+    text('N', 20,100, r);
+    text('o', 127,30, r);
+    text('n', 127,40, r);
+    text('i', 127,50, r);
+    text('c', 127,60, r);
+    text('a', 127,70, r);
+    text('H', 118,70, r);
+    text('v', 134,70, r);
+    text('e', 140,70, r);
+    text('y', 151,70, r);
+    text('o', 157,70, r);
+    text('u', 164,70, r);
+    text('b', 174,70, r);
+    text('e', 181,70, r);
+    text('e', 188,70, r);
+    text('n', 195,70, r);
+    text('g', 205,70, r);
+    text('o', 212,70, r);
+    text('i', 219,70, r);
+    text('n', 222,70, r);
+    text('g', 229,70, r);
+    text('t', 239,70, r);
+    text('o', 243,70, r);
+    text('t', 253,70, r);
+    text('h', 257,70, r);
+    text('e', 264,70, r);
+    text('g', 274,70, r);
+    text('y', 281,70, r);
+    text('m', 287,70, r);
+    text('a', 188,80, r);
+    text('t', 188,90, r);
+    text('M', 188,105, r);
+    text('e', 188,113, r);
 
  
   }
